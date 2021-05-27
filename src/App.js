dc77c1f5be4ba3afa2f83b6cd3db18c6468c426e
Shubham Arya <79799687+bunnyarya@users.noreply.github.com>
2021-05-27 19:26:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/Navbar';
+//import Navbar from './Components/Navbar';
+import Like from './Components/Like';
+import Dislike from './Components/Unlike'
 
 function App() {
   return (
-
+    
+  <div className="App-header">
     <div className="App">
-      <header className="App-header">
-        <Navbar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
-      </header>
+      <div className="inline">
+          <ul>
+            <li>
+              <Like />
+            </li>
+            <li>
+              <Dislike />
+            </li>
+          </ul>
+      </div>
     </div>
+  </div>
   );
 }
 
