e303ed558cb0bf0f58436c9cf144168aa984fc22
Shubham Arya <79799687+bunnyarya@users.noreply.github.com>
2021-05-29 17:15:03 +0530

    Update Articles.js

--- src/Articles.js
+++ src/Articles.js
@@ -1,25 +1,186 @@
 import "./App.css";
 import Like from "./Components/Like";
 import Dislike from "./Components/Unlike";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import Article from "./Article";
 
-function Article() {
+function Articles() {
     return (
-        <div className="App-header">
-            <div className="App">
-                <p>lorem50</p>
-                <div className="inline">
-                    <ul>
-                        <li>
-                            <Like />
-                        </li>
-                        <li>
-                            <Dislike />
-                        </li>
-                    </ul>
+        <Router>
+            <div className="App-header">
+                <div className="App">
+                    <div style={{ marginTop: 1.5 + 'em' }}>
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="column">
+                            <Link to='/Article'>
+                                <h2>Column</h2>
+                            </Link>
+                            <p align="justify">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+                                sit amet pretium urna. Vivamus venenatis velit nec neque
+                                ultricies, eget elementum magna tristique. Quisque vehicula, risus
+                                eget aliquam placerat, purus leo tincidunt eros, eget luctus quam
+                                orci in velit. Praesent scelerisque tortor sed accumsan convallis.
+                        </p>
+                            <div className="inline">
+                                <ul>
+                                    <li>
+                                        <Like />
+                                    </li>
+                                    <li>
+                                        <Dislike />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Router>
     );
 }
 
-export default Article;
+export default Articles;
