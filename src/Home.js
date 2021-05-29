0a075d14d99bfeee9a8feb891f1c4c3d29087492
Shubham Arya <79799687+bunnyarya@users.noreply.github.com>
2021-05-29 17:15:46 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,37 @@ function Home() {
     return (
         <div className="App-header">
             <div className="App">
-                <p><img src={logo} className="App-logo" alt="logo" /></p>
+                <img src={logo} className="App-logo" alt="logo" />
+
+                <div class="para" >
+                    <div class="trending">
+                        <p>
+                            <img src="https://image.freepik.com/free-photo/black-brick-wall-textured-background_53876-63572.jpg" class="responsive" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            sit amet pretium urna. Vivamus venenatis velit nec neque
+                        </p>
+                    </div>
+                    <div class="recent">
+                        <p>
+                            <img src="https://image.freepik.com/free-photo/black-brick-wall-textured-background_53876-63572.jpg" class="responsive" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            sit amet pretium urna. Vivamus venenatis velit nec neque
+                        </p>
+
+                        <p>
+                            <img src="https://image.freepik.com/free-photo/black-brick-wall-textured-background_53876-63572.jpg" class="responsive" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            sit amet pretium urna. Vivamus venenatis velit nec neque
+                        </p>
+
+                        <p>
+                            <img src="https://image.freepik.com/free-photo/black-brick-wall-textured-background_53876-63572.jpg" class="responsive" />
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            sit amet pretium urna. Vivamus venenatis velit nec neque
+                        </p>
+
+                    </div>
+                </div>
             </div>
         </div>
     );
